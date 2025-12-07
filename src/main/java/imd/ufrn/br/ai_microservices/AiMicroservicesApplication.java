1f0f1fc22022fc0f614d426b4e06ef692c9b04b9
Jorgelino328 <jorgewilliam328@gmail.com>
2025-12-07 14:04:28 -0300

    initial commit

---
+++ src/main/java/imd/ufrn/br/ai_microservices/AiMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package imd.ufrn.br.ai_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/imd/ufrn/br/ai_microservices/AiMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package imd.ufrn.br.ai_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
